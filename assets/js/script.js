c91136d07acfdbdf1199dbf86815c0479d7796f6
Wendy Tekverk <wtekverk@gmail.com>
2021-02-01 21:03:34 -0700

    added weather information to js to conect to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
+$(document).ready(function(){
 
 var apiKey = `4d2c3213cbd1a166176ccac96a5f601b`
 
 
-function getForecast(cityInput){
+function getForecast(cityInput) {
 
     var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response){
+    }).then(function (response) {
 
-    var response = response
+        var response = response
 
-  
-    console.log(response)
+        console.log(response)
 
-//loop through data for card
+
 
     })
 
@@ -25,22 +25,38 @@ function getForecast(cityInput){
 
 
 }
-function getWeather(cityInput){
+
+function getWeather(cityInput) {
 
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response){
+    })
+    .then(function (response) {
 
-    var response = response
-var lat = response.coord.lat
-var long = response.coord.long
-    console.log(response)
+        var response = response
 
-getUV(lat, long)
-//loop through data for card
+        var temp = (response.main.temp - 273.15) * 1.8 + 32
+
+        $("#temperature").text("Temperature (F): " + temp.toFixed(2) + "°");
+        $("#humidity").text("Humidity: " + response.main.humidity);
+        $("#wind_speed").text("Wind Speed: " + response.wind.speed);
+        $("#city-name").text(response.name)
+        $("#weather-pict").attr("src", response.weather.icon)
+
+
+
+        var lat = response.coord.lat
+        var long = response.coord.lon
+       
+        console.log(lat)
+        console.log(long)
+
+        getUV(lat, long)
+
+
 
     })
 
@@ -49,21 +65,23 @@ getUV(lat, long)
 
 
 }
-function getUV(lat, long){
 
-    var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey}`
+function getUV(lat, long) {
+
+    var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response){
+    }).then(function (response) {
 
-    var response = response
+        var response = response
 
-    
-    console.log(response)
-
-//loop through data for card
+        console.log(response)
+        
+        $("#uv_index").text("UV Index: " + response.current.uvi);
+        $("#weather-pict").text(response.hourly.weather.icon)
+        
 
     })
 
@@ -77,34 +95,29 @@ function getUV(lat, long){
 
 //FUNC: search city prompt current and future conditions
 
-$('#search').on("click", function (){
+$('#search').on("click", function () {
 
     var cityInput = $("#city-input").val()
 
     getForecast(cityInput)
+
     getWeather(cityInput)
 
 
 
-
-
-//add call to search history (so set local storage)
+    //add call to search history (so set local storage)
 
 
 })
 
 
-//weather search
-    //list city name , date , weather icon, conditions, temp and humidity
 
 
-    //Uv index <> color change
 
+//Uv index <> color change
 
-//5 day weather forecast
-
-
-//search history buttons 
+ 
 
 
 //present with last search on reload
+})
