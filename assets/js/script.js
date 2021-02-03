71435568861040bbc2a2c441ea507d1e273559b0
Wendy Tekverk <wtekverk@gmail.com>
2021-02-02 19:41:27 -0700

    adding local storage and history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-    var apiKey = `4d2c3213cbd1a166176ccac96a5f601b`
-
+    var apiKey = `fc5763942c8367963720f177bcb72659`
+    var previousSearch = JSON.parse(localStorage.getItem("city")) || []
 
     function getForecast(lat, long) {
 
@@ -11,7 +11,6 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            
             var response = response
             console.log(response)
 
@@ -27,47 +26,40 @@ $(document).ready(function () {
             var day5 = month + "/" + (dayOfMonth + 5)
 
 
-            // day one commands 
             $("#date1").text("Date: " + day1);
             var iconCode1 = response.daily[0].weather[0].icon;
-            var iconURL1 = "http://openweathermap.org/img/w/" + iconCode1 + ".png";
+            var iconURL1 = "https://openweathermap.org/img/w/" + iconCode1 + ".png";
             $('#icon1').attr('src', iconURL1);
             $("#temp1").text("Temp: " + parseFloat(((response.daily[0].temp.day) - 273.15) * (9 / 5) + 32).toFixed(2) + "°F");
             $("#hum1").text("Humidity: " + response.current.humidity + "%");
-            
-            // day two commands 
+
             $("#date2").text("Date: " + day2);
             var iconCode2 = response.daily[1].weather[0].icon;
-            var iconURL2 = "http://openweathermap.org/img/w/" + iconCode2 + ".png";
+            var iconURL2 = "https://openweathermap.org/img/w/" + iconCode2 + ".png";
             $('#icon2').attr('src', iconURL2);
             $("#temp2").text("Temp: " + parseFloat(((response.daily[1].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
             $("#hum2").text("Humidity: " + response.current.humidity + "%");
 
-            // day three commands 
             $("#date3").text("Date: " + day3);
             var iconCode3 = response.daily[2].weather[0].icon;
-            var iconURL3 = "http://openweathermap.org/img/w/" + iconCode3 + ".png";
+            var iconURL3 = "https://openweathermap.org/img/w/" + iconCode3 + ".png";
             $('#icon3').attr('src', iconURL3);
             $("#temp3").text("Temp: " + parseFloat(((response.daily[2].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
             $("#hum3").text("Humidity: " + response.current.humidity + "%");
 
-            // day four commands 
             $("#date4").text("Date: " + day4);
             var iconCode4 = response.daily[3].weather[0].icon;
-            var iconURL4 = "http://openweathermap.org/img/w/" + iconCode4 + ".png";
+            var iconURL4 = "https://openweathermap.org/img/w/" + iconCode4 + ".png";
             $('#icon4').attr('src', iconURL4);
             $("#temp4").text("Temp: " + parseFloat(((response.daily[3].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
             $("#hum4").text("Humidity: " + response.current.humidity + "%");
 
-            // day five commands 
             $("#date5").text("Date: " + day5);
             var iconCode5 = response.daily[4].weather[0].icon;
-            var iconURL5 = "http://openweathermap.org/img/w/" + iconCode5 + ".png";
+            var iconURL5 = "https://openweathermap.org/img/w/" + iconCode5 + ".png";
             $('#icon5').attr('src', iconURL5);
             $("#temp5").text("Temp: " + parseFloat(((response.daily[4].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
             $("#hum5").text("Humidity: " + response.current.humidity + "%");
-
-
 
 
         })
@@ -88,7 +80,14 @@ $(document).ready(function () {
                 var response = response
 
                 console.log(response)
+                if (previousSearch.indexOf(cityInput) === -1) {
 
+
+                    previousSearch.push(cityInput)
+                    localStorage.setItem("city", JSON.stringify(previousSearch))
+
+                    displayLocalStorage();
+                }
                 var temp = (response.main.temp - 273.15) * 1.8 + 32
 
                 $("#temperature").text("Temperature (F): " + temp.toFixed(2) + "°");
@@ -98,7 +97,7 @@ $(document).ready(function () {
                 $("#weather-pict").attr("src", response.weather.icon)
 
                 var iconCode = response.weather[0].icon;
-                var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
                 $('#weather-pict').attr('src', iconURL);
 
 
@@ -111,15 +110,11 @@ $(document).ready(function () {
 
             })
 
-
-
-
-
     }
 
     function getUV(lat, long) {
 
-        var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`
+        var queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey}`
 
         $.ajax({
             url: queryURL,
@@ -127,41 +122,45 @@ $(document).ready(function () {
         }).then(function (response) {
 
             var response = response
-
-
-
-            $("#uv_index").text("UV Index: " + response.current.uvi);
-
-
+          console.log(response)
+            $("#uv_index").text("UV Index: " + response.value);
 
         })
 
-
-
-
-
     }
+
 
     $('#search').on("click", function () {
 
         var cityInput = $("#city-input").val()
 
-
-
         getWeather(cityInput)
-
 
         localStorage.setItem("count", cityInput)
 
-        $("history-btns").prepend("<button>" + cityInput)
 
+
+        // $("history-btns").prepend("<button>" + cityInput)
 
 
     })
 
 
+    function displayLocalStorage() {
+        var previousSearch = JSON.parse(localStorage.getItem("city")) || [];
+        var html = ''
+        for (var i = 0; i < previousSearch.length; i++) {
+            html += `<button class = "btn btn-primary cityName">${previousSearch[i]}</button>`
+        }
+        console.log(html)
+        $("#history_btn").html(html)
+    }
 
+    $("#history_btn").on("click", ".cityName", function () {
+        var randomName = $(this).text()
+        console.log(randomName)
+        getWeather(randomName);
+    })
 
-
-
+    displayLocalStorage();
 })
