c2881e1857eb9303281b57cfad74fb8cc8aaff99
Wendy Tekverk <wtekverk@gmail.com>
2021-02-02 18:17:07 -0700

    stylizing forcast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     var apiKey = `4d2c3213cbd1a166176ccac96a5f601b`
 
 
-
     function getForecast(lat, long) {
 
         var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`
@@ -12,6 +11,7 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            
             var response = response
             console.log(response)
 
@@ -27,40 +27,45 @@ $(document).ready(function () {
             var day5 = month + "/" + (dayOfMonth + 5)
 
 
-            $(".date1").text("Date: " + day1);
+            // day one commands 
+            $("#date1").text("Date: " + day1);
             var iconCode1 = response.daily[0].weather[0].icon;
             var iconURL1 = "http://openweathermap.org/img/w/" + iconCode1 + ".png";
-            $('.icon1').attr('src', iconURL1);
-            $(".temp1").text("Temp: " + parseFloat(((response.daily[0].temp.day) - 273.15) * (9 / 5) + 32).toFixed(2) + "°F");
-            $(".hum1").text("Humidity: " + response.current.humidity + "%");
-
-            $(".date2").text("Date: " + day2);
+            $('#icon1').attr('src', iconURL1);
+            $("#temp1").text("Temp: " + parseFloat(((response.daily[0].temp.day) - 273.15) * (9 / 5) + 32).toFixed(2) + "°F");
+            $("#hum1").text("Humidity: " + response.current.humidity + "%");
+            
+            // day two commands 
+            $("#date2").text("Date: " + day2);
             var iconCode2 = response.daily[1].weather[0].icon;
             var iconURL2 = "http://openweathermap.org/img/w/" + iconCode2 + ".png";
-            $('.icon2').attr('src', iconURL2);
-            $(".temp2").text("Temp: " + parseFloat(((response.daily[1].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
-            $(".hum2").text("Humidity: " + response.current.humidity + "%");
+            $('#icon2').attr('src', iconURL2);
+            $("#temp2").text("Temp: " + parseFloat(((response.daily[1].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
+            $("#hum2").text("Humidity: " + response.current.humidity + "%");
 
-            $(".date3").text("Date: " + day3);
+            // day three commands 
+            $("#date3").text("Date: " + day3);
             var iconCode3 = response.daily[2].weather[0].icon;
             var iconURL3 = "http://openweathermap.org/img/w/" + iconCode3 + ".png";
-            $('.icon3').attr('src', iconURL3);
-            $(".temp3").text("Temp: " + parseFloat(((response.daily[2].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
-            $(".hum3").text("Humidity: " + response.current.humidity + "%");
+            $('#icon3').attr('src', iconURL3);
+            $("#temp3").text("Temp: " + parseFloat(((response.daily[2].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
+            $("#hum3").text("Humidity: " + response.current.humidity + "%");
 
-            $(".date4").text("Date: " + day4);
+            // day four commands 
+            $("#date4").text("Date: " + day4);
             var iconCode4 = response.daily[3].weather[0].icon;
             var iconURL4 = "http://openweathermap.org/img/w/" + iconCode4 + ".png";
-            $('.icon4').attr('src', iconURL4);
-            $(".temp4").text("Temp: " + parseFloat(((response.daily[3].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
-            $(".hum4").text("Humidity: " + response.current.humidity + "%");
+            $('#icon4').attr('src', iconURL4);
+            $("#temp4").text("Temp: " + parseFloat(((response.daily[3].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
+            $("#hum4").text("Humidity: " + response.current.humidity + "%");
 
-            $(".date5").text("Date: " + day5);
+            // day five commands 
+            $("#date5").text("Date: " + day5);
             var iconCode5 = response.daily[4].weather[0].icon;
             var iconURL5 = "http://openweathermap.org/img/w/" + iconCode5 + ".png";
-            $('.icon5').attr('src', iconURL5);
-            $(".temp5").text("Temp: " + parseFloat(((response.daily[4].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
-            $(".hum5").text("Humidity: " + response.current.humidity + "%");
+            $('#icon5').attr('src', iconURL5);
+            $("#temp5").text("Temp: " + parseFloat(((response.daily[4].temp.day) - 273.15) * (9 / 5) + 32).toFixed(0) + "°F");
+            $("#hum5").text("Humidity: " + response.current.humidity + "%");
 
 
 
